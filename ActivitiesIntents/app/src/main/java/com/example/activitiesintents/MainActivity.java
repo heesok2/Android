2467c6b65fd3a451812c32c6705839c9ecedfabf
heesok2 <heesok2@gmail.com>
2019-04-03 08:59:54 +0900

    Activities and Intents - Study Sample Code

--- ActivitiesIntents/app/src/main/java/com/example/activitiesintents/MainActivity.java
+++ ActivitiesIntents/app/src/main/java/com/example/activitiesintents/MainActivity.java
@@ -1,18 +1,51 @@
 package com.example.activitiesintents;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_SEND = "com.example.activitiesIntents.extra.send";
+    public static final int REQUEST_CODE = 1;
+    // Message
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    // Control
+    private TextView mTextViewReply;
+    private EditText mEditTextSend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(LOG_TAG, "MainActivity:onCreate");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextViewReply = findViewById(R.id.textView_reply);
+        mEditTextSend = findViewById(R.id.editText_send);
     }
 
-    public void OnClikedSend(View view) {
+    public void onClickedSend(View view) {
+        Log.d(LOG_TAG, "MainActivity:onClickedSend");
 
+        String strMsg = mEditTextSend.getText().toString();
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra(EXTRA_SEND, strMsg);
+        startActivityForResult(intent, REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        Log.d(LOG_TAG, "MainActivity:onActivityResult");
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE) {
+            String strMsg = data.getStringExtra(SecondActivity.EXTRA_REPLY);
+            mTextViewReply.setText(strMsg);
+        }
     }
 }
--- ActivitiesIntents/app/src/main/java/com/example/activitiesintents/SecondActivity.java
+++ ActivitiesIntents/app/src/main/java/com/example/activitiesintents/SecondActivity.java
@@ -1,18 +1,42 @@
 package com.example.activitiesintents;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+    public static final String EXTRA_REPLY = "com.example.activitiesIntents.extra.reply";
+    private static final String LOG_TAG = SecondActivity.class.getSimpleName();
+    private TextView mTextViewSend;
+    private EditText mEditTextReply;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(LOG_TAG, "SecondActivity:onCreate");
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second2);
+        setContentView(R.layout.activity_second);
+
+        mTextViewSend = findViewById(R.id.textView_send);
+        mEditTextReply = findViewById(R.id.editText_reply);
+
+        Intent intent = getIntent();
+        String strMsg = intent.getStringExtra(MainActivity.EXTRA_SEND);
+
+        mTextViewSend.setText(strMsg);
     }
 
-    public void OnClickedReply(View view) {
+    public void onClickedReply(View view) {
+        Log.d(LOG_TAG, "SecondActivity:onClickedReply");
 
+        String strMsg = mEditTextReply.getText().toString();
+        Intent intent = new Intent();
+        intent.putExtra(EXTRA_REPLY, strMsg);
+        setResult(RESULT_OK, intent);
+        finish();
     }
 }
