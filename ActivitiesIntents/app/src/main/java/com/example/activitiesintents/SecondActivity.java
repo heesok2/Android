48e84734ce0a7be10858024e83f6048890026e3d
heesok2 <heesok2@gmail.com>
2019-04-02 14:27:56 +0900

    Activities and Intends

---
+++ ActivitiesIntents/app/src/main/java/com/example/activitiesintents/SecondActivity.java
@@ -0,0 +1,18 @@
+package com.example.activitiesintents;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second2);
+    }
+
+    public void OnClickedReply(View view) {
+
+    }
+}
